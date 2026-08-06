25427de3e68814aa59595994d1b6039173c17594
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-06 19:36:58 +0100

    fixed first page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,67 +1,166 @@
-import Image from "next/image";
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+type formType = {
+  username?: string;
+  email: string;
+  password: string;
+};
 
 export default function Home() {
+  const [signUpForm, setSignUpForm] = useState<formType>({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [signInForm, setSignInForm] = useState<formType>({
+    email: "",
+    password: "",
+  });
+  const supabase = createClient();
+  const router = useRouter();
+
+  const handleSignUpChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setSignUpForm({ ...signUpForm, [event.target.name]: event.target.value });
+  };
+
+  const handleSignInChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setSignInForm({ ...signInForm, [event.target.name]: event.target.value });
+  };
+
+  const handleSignUp = async (
+    event: React.ChangeEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    const { data, error } = await supabase.auth.signUp({
+      email: signUpForm.email,
+      password: signUpForm.password,
+      options: {
+        emailRedirectTo: `${window.location.origin}/auth/callback?next=/dashboard`,
+        data: {
+          username: signUpForm.username,
+        },
+      },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignIn = async (
+    event: React.ChangeEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: signInForm.email,
+      password: signInForm.password,
+    });
+
+    router.push("/dashboard");
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div>
+      <main>
+        <h1>Sign UP and Sign In Page</h1>
+        <div>
+          <h2>Sign Up</h2>
+          <div>
+            <form
+              onSubmit={(event: React.ChangeEvent<HTMLFormElement>) =>
+                handleSignUp(event)
+              }
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              <div>
+                <label>Username: </label>
+                <input
+                  type="text"
+                  value={signUpForm.username}
+                  name="username"
+                  required
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleSignUpChange(event)
+                  }
+                />
+              </div>
+              <div>
+                <label>E-Mail: </label>
+                <input
+                  type="email"
+                  value={signUpForm.email}
+                  name="email"
+                  required
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleSignUpChange(event)
+                  }
+                />
+              </div>
+              <div>
+                <label>Password: </label>
+                <input
+                  type="password"
+                  value={signUpForm.password}
+                  name="password"
+                  required
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleSignUpChange(event)
+                  }
+                />
+              </div>
+              <button>Sign Up</button>
+            </form>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div>
+          <h2>Sign In</h2>
+          <div>
+            <form
+              onSubmit={(event: React.ChangeEvent<HTMLFormElement>) =>
+                handleSignIn(event)
+              }
+            >
+              <div>
+                <label>E-Mail: </label>
+                <input
+                  type="text"
+                  name="email"
+                  value={signInForm.email}
+                  required
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleSignInChange(event)
+                  }
+                />
+              </div>
+              <div>
+                <label>Password: </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={signInForm.password}
+                  required
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleSignInChange(event)
+                  }
+                />
+              </div>
+              <button>Sign In</button>
+            </form>
+          </div>
         </div>
       </main>
     </div>
