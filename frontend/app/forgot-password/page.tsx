c47d14a95b0499f25d1ff4177e0ba8e62d706dd9
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-07 14:34:09 +0100

    forgot password

---
+++ frontend/app/forgot-password/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useState } from "react";
+
+const ForgotPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const supabase = createClient();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = async (
+    event: React.ChangeEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    (await supabase).auth.resetPasswordForEmail(email, {
+      redirectTo: `${window.location.origin}/reset-password`,
+    });
+  };
+  return (
+    <div>
+      <h1>Forgot Password Page</h1>
+      <p>Input your email</p>
+      <form
+        onSubmit={(event: React.ChangeEvent<HTMLFormElement>) =>
+          handleSubmit(event)
+        }
+      >
+        <div>
+          <label>E-mail: </label>
+          <input
+            type="email"
+            name="email"
+            required
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleChange(event)
+            }
+          />
+        </div>
+        <button>Forgot Password</button>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPage;
