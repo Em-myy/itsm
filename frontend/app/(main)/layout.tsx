8c8ade5215273830c80f658699404e9bc5d2a62f
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-11 22:08:49 +0100

    fixed some issues

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { AuthProvider } from "@/context/AuthContext";
 import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "IT Service Management",
@@ -12,7 +13,10 @@ export default function MainLayout({ children }: LayoutProps<"/">) {
     <div className="min-h-full flex">
       <AuthProvider>
         <Sidebar />
-        {children}
+        <div>
+          <Navbar />
+          {children}
+        </div>
       </AuthProvider>
     </div>
   );
