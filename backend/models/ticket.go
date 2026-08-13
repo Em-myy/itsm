ec2065d4c4ca12cc6e6ee865c6d3cb60c5213a82
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-13 22:17:41 +0100

    ticket

---
+++ backend/models/ticket.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Ticket struct {
+	ID           int       `json:"id"`
+	Title        string    `json:"title"`
+	Category     string    `json:"category"`
+	Department   string    `json:"department"`
+	Priority     string    `json:"priority"`
+	RelatedAsset string    `json:"related_asset"`
+	Description  string    `json:"description"`
+	RequesterId  string    `json:"requester_id"`
+	AssigneeId   *string   `json:"assignee_id"`
+	Picture      []string  `json:"picture"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
