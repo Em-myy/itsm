7caf5b2f847d91c4ab408d09d179c2c448743b6c
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-15 21:15:16 +0100

    fourth

---
+++ backend/repositories/ticket_repo.go
@@ -0,0 +1,110 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+	"itsm/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateTicket(ctx context.Context, pool *pgxpool.Pool, ticket models.Ticket) (int, error) {
+	query := `
+		INSERT INTO tickets (id, title, category, department, priority, related_asset, description, requester_id, assignee_id, picture)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		RETURNING id
+		`
+	var newID int
+	err := pool.QueryRow(
+		ctx,
+		query,
+		ticket.Title,
+		ticket.Category,
+		ticket.Department,
+		ticket.Priority,
+		ticket.RelatedAsset,
+		ticket.Description,
+		ticket.RequesterId,
+		ticket.AssigneeId,
+		ticket.Picture,
+	).Scan(&newID)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to create ticket: %w", err)
+	}
+	return newID, nil
+}
+
+func GetTickets(ctx context.Context, pool *pgxpool.Pool) ([]models.Ticket, error) {
+	query := `
+		SELECT title, category, department, priority, related_asset, description, requester_id, assignee_id, picture, created_at, updated_at
+		FROM tickets
+		ORDER BY created_at DESC
+		`
+	rows, err := pool.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to query tickets: %w", err)
+	}
+	defer rows.Close()
+
+	var tickets []models.Ticket
+	for rows.Next() {
+		var t models.Ticket
+		err := rows.Scan(
+			&t.ID,
+			&t.Title,
+			&t.Category,
+			&t.Department,
+			&t.Priority,
+			&t.RelatedAsset,
+			&t.Description,
+			&t.RequesterId,
+			&t.AssigneeId,
+			&t.Picture,
+			&t.CreatedAt,
+			&t.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to scan ticket row, %w", err)
+		}
+		tickets = append(tickets, t)
+	}
+	return tickets, nil
+}
+
+func GetTicketsByRequester(ctx context.Context, pool *pgxpool.Pool, requesterID string) ([]models.Ticket, error) {
+	query := `
+		SELECT id, title, category, department, priority, related_asset, description, requester_id, assignee_id, picture, created_at, updated_at
+		FROM tickets
+		WHERE requesterId == $1
+		ORDER BY created_at DESC
+		`
+	rows, err := pool.Query(ctx, query, requesterID)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to query user tickets: %w", err)
+	}
+	defer rows.Close()
+
+	var tickets []models.Ticket
+	for rows.Next() {
+		var t models.Ticket
+		err := rows.Scan(
+			&t.ID,
+			&t.Title,
+			&t.Category,
+			&t.Department,
+			&t.Priority,
+			&t.RelatedAsset,
+			&t.Description,
+			&t.RequesterId,
+			&t.AssigneeId,
+			&t.Picture,
+			&t.CreatedAt,
+			&t.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to scan ticket row, %w", err)
+		}
+		tickets = append(tickets, t)
+	}
+	return tickets, nil
+}
