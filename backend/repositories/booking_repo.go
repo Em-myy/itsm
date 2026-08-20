07a2b947020e15a24a57f689580c87c4fa5257bc
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-20 22:03:25 +0100

    booking repo

---
+++ backend/repositories/booking_repo.go
@@ -0,0 +1,105 @@
+package repositories
+
+import (
+	"context"
+	"fmt"
+	"itsm/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateBooking(ctx context.Context, pool *pgxpool.Pool, booking models.Booking) (int, error) {
+	query := `
+		INSERT INTO bookings (user_id, purpose, venue, start_time, end_time, equipment_needed, status)
+		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		RETURNING id;
+	`
+	var newId int
+
+	err := pool.QueryRow(
+		ctx,
+		query,
+		booking.UserId,
+		booking.Purpose,
+		booking.Venue,
+		booking.StartTime,
+		booking.EndTime,
+		booking.EquipmentNeeded,
+		booking.Status,
+	).Scan(&newId)
+	if err != nil {
+		return 0, fmt.Errorf("Failed to create new booking: %w", err)
+	}
+	return newId, nil
+}
+
+func GetBookings(ctx context.Context, pool *pgxpool.Pool) ([]models.Booking, error) {
+	query := `
+		SELECT id, user_id, purpose, venue, start_time, end_time, equipment_needed, status, created_at, updated_at
+		FROM bookings
+		ORDER BY start_time ASC;
+	`
+	rows, err := pool.Query(ctx, query)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to query bookings: %w", err)
+	}
+	defer rows.Close()
+
+	var bookings []models.Booking
+	for rows.Next() {
+		var b models.Booking
+		err := rows.Scan(
+			&b.ID,
+			&b.UserId,
+			&b.Purpose,
+			&b.Venue,
+			&b.StartTime,
+			&b.EndTime,
+			&b.EquipmentNeeded,
+			&b.Status,
+			&b.CreatedAt,
+			&b.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to scan bookings row: %w", err)
+		}
+		bookings = append(bookings, b)
+	}
+	return bookings, nil
+}
+
+func GetBookingByRequester(ctx context.Context, pool *pgxpool.Pool, userId string) ([]models.Booking, error) {
+	query := `
+		SELECT id, user_id, purpose, venue, start_time, end_time, equipment_needed, status, created_at, updated_at
+		FROM bookings
+		WHERE user_id = $1
+		ORDER BY start_time ASC;
+	`
+	rows, err := pool.Query(ctx, query, userId)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to query user bookings: %w", err)
+	}
+	defer rows.Close()
+
+	var bookings []models.Booking
+	for rows.Next() {
+		var b models.Booking
+		err := rows.Scan(
+			&b.ID,
+			&b.UserId,
+			&b.Purpose,
+			&b.Venue,
+			&b.StartTime,
+			&b.EndTime,
+			&b.EquipmentNeeded,
+			&b.Status,
+			&b.CreatedAt,
+			&b.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to scan bookings row: %w", err)
+		}
+		bookings = append(bookings, b)
+	}
+	return bookings, nil
+}
