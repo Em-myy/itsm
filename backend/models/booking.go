35a581acc41e57472d82aa91201c6f3dab92d4dd
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-21 23:08:34 +0100

    booking model

--- backend/models/booking.go
+++ backend/models/booking.go
@@ -6,11 +6,22 @@ type Booking struct {
 	ID              int       `json:"id"`
 	UserId          string    `json:"user_id"`
 	Purpose         string    `json:"purpose"`
-	Venue           string    `json:"Venue"`
+	VenueID         int       `json:"venue_id"`
+	VenueName       string    `json:"venue_name,omitempty"`
 	StartTime       time.Time `json:"start_time"`
 	EndTime         time.Time `json:"end_time"`
 	EquipmentNeeded []string  `json:"equipment_needed"`
 	Status          string    `json:"status"`
 	CreatedAt       time.Time `json:"created_at"`
 	UpdatedAt       time.Time `json:"updated_at"`
+}
+
+type Venue struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Capacity   int       `json:"capacity"`
+	Status     string    `json:"status"`
+	Equipments []string  `json:"equipments"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
