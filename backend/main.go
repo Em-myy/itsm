dd6ad41f7dd3eda73c55232038c0b21956d81b94
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-24 23:50:10 +0100

    Refactor main function to improve environment variable handling and update server startup message

--- backend/main.go
+++ backend/main.go
@@ -3,30 +3,27 @@ package main
 import (
 	"fmt"
 	"itsm/database"
-	"itsm/middleware"
 	"itsm/routes"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
-
 	if err != nil {
-		log.Fatal("Error loading .env")
+		log.Println("No .env file found, relying on system environment variable")
 	}
 
 	pool, err := database.Connect()
-
 	if err != nil {
 		log.Fatal("Database connection failed: ", err)
 	}
-
 	defer pool.Close()
 
-	fmt.Println("Connected to postgres database")
+	fmt.Println("Connected to Postgres database")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "ITSM backend is running")
@@ -34,11 +31,14 @@ func main() {
 
 	router := routes.SetupRouter(pool)
 
-	fmt.Println("Server is running on http://localhost:8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	handler := middleware.CorsMiddleware(router)
+	fmt.Printf("Server is running on port %s/n", port)
 
-	if err := http.ListenAndServe(":8080", handler); err != nil {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatal(err)
 	}
 }
