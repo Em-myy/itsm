814fe4aa6dce718b95350359b260e74a1dbd6deb
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-26 13:07:49 +0100

    Refactor Navbar component to use role from AuthContext and remove unused role fetching logic

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useAuth } from "@/context/AuthContext";
-import api from "@/src/lib/axios";
-import { useEffect, useState } from "react";
 
 interface RoleType {
   id: Number;
@@ -11,20 +9,8 @@ interface RoleType {
 }
 
 const Navbar = () => {
-  const [role, setRole] = useState<RoleType | null>(null);
-  const { avatar, initials, displayName } = useAuth();
+  const { avatar, initials, displayName, role } = useAuth();
 
-  useEffect(() => {
-    const fetchRoles = async (): Promise<void> => {
-      try {
-        const response = await api.get("/role");
-        setRole(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchRoles();
-  }, []);
   return (
     <div className="w-5xl">
       <div className="flex justify-between">
