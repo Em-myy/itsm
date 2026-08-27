84bac7a5fdc6e6acca38653982093a0a5d8dda2d
Emmy <famuyiwaemmanuel565@gmail.com>
2026-08-27 01:47:48 +0100

    Add return type to getRole function in AuthProvider

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -37,7 +37,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
   useEffect(() => {
-    const getRole = async () => {
+    const getRole = async (): Promise<void> => {
       if (!user) {
         return;
       }
