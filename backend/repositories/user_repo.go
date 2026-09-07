00e22e7110ac5a58cc2adf69421d4e8f545c17df
Emmy <famuyiwaemmanuel565@gmail.com>
2026-09-07 01:04:38 +0100

    Refactor UpdateUserProfile function: change parameters to improve flexibility and maintainability

--- backend/repositories/user_repo.go
+++ backend/repositories/user_repo.go
@@ -8,26 +8,57 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func UpdateUserProfile(ctx context.Context, pool *pgxpool.Pool, user models.User) error {
+func UpdateUserProfile(
+	ctx context.Context,
+	pool *pgxpool.Pool,
+	userID string,
+	username *string,
+	department *string,
+) error {
 	query := `
 		UPDATE users 
-		SET department = $1,
-			username = $2,
-			updated_at = CURRENT_TIMESTAMP
+		SET 
+			username = COALESCE($1, username),
+			department = COALESCE($2, department)
+			updated_at = NOW()
 		WHERE id = $3
 		`
 	commandTag, err := pool.Exec(
 		ctx,
 		query,
-		user.Department,
-		user.Username,
-		user.ID)
+		username,
+		department,
+		userID,
+	)
 	if err != nil {
 		return fmt.Errorf("Failed to update user profile: %w", err)
 	}
 
 	if commandTag.RowsAffected() == 0 {
-		return fmt.Errorf("No user found with ID: %s", user.ID)
+		return fmt.Errorf("No user found with ID: %s", userID)
 	}
 	return nil
+}
+
+func GetUserProfile(ctx context.Context, pool *pgxpool.Pool, userID string) (*models.User, error) {
+	query := `
+		SELECT id, username, department, role, created_at, updated_at
+		FROM users
+		WHERE id = $1;
+	`
+	var user models.User
+	err := pool.QueryRow(ctx, query, userID).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Department,
+		&user.RoleId,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get user profile: %w", err)
+	}
+
+	return &user, nil
 }
