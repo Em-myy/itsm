80687769a875cbc6118feefc9c483739b64f6928
Emmy <famuyiwaemmanuel565@gmail.com>
2026-09-10 19:36:46 +0100

    feat: implement UserPage component to fetch and display users

--- frontend/app/(main)/admin/users/page.tsx
+++ frontend/app/(main)/admin/users/page.tsx
@@ -1,9 +1,11 @@
-const UsersPage = () => {
-  return (
-    <div>
-      <h1>This is the users page</h1>
-    </div>
-  );
+import { fetchFromGo } from "@/lib/api-server";
+import { UserType } from "@/lib/types";
+import UserClientPage from "./UserClientPage";
+
+const UserPage = async () => {
+  const users = (await fetchFromGo("/users")) as UserType[] | [];
+
+  return <UserClientPage initialUsers={users} />;
 };
 
-export default UsersPage;
+export default UserPage;
