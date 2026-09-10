dde1fb062c7acf025e21fb45d3f5fe2f05a12176
Emmy <famuyiwaemmanuel565@gmail.com>
2026-09-10 23:54:58 +0100

    feat: include email in user profile display on UserClientPage

--- frontend/app/(main)/admin/users/UserClientPage.tsx
+++ frontend/app/(main)/admin/users/UserClientPage.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { useAuth } from "@/context/AuthContext";
-import api from "@/lib/axios";
-import { ProfileType, UserType } from "@/lib/types";
-import { useEffect } from "react";
+import { UserType } from "@/lib/types";
 
 const UserClientPage = ({ initialUsers }: { initialUsers: UserType[] }) => {
   return (
     <div>
       <h1>This is the users page</h1>
-      {initialUsers.map((profile) => (
-        <div key={profile.id}>
-          <div>{profile.username}</div>
-          <div>{profile.department}</div>
-          <div>{profile.role_name}</div>
+      {initialUsers.map((user) => (
+        <div key={user.id}>
+          <div>{user.username}</div>
+          <div>{user.email}</div>
+          <div>{user.department}</div>
+          <div>{user.role_name}</div>
         </div>
       ))}
     </div>
