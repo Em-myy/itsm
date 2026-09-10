aafae56ce09e6ceb7f75fa309a2e378b558afea5
Emmy <famuyiwaemmanuel565@gmail.com>
2026-09-10 23:55:06 +0100

    refactor: simplify ProfilePage by removing unused supabase client and email handling

--- frontend/app/(main)/profile/page.tsx
+++ frontend/app/(main)/profile/page.tsx
@@ -1,29 +1,11 @@
 import { fetchFromGo } from "@/lib/api-server";
-import { ProfileType } from "@/lib/types";
 import ProfileClient from "./ProfileClient";
-import { createClient } from "@/utils/supabase/server";
+import { UserType } from "@/lib/types";
 
 const ProfilePage = async () => {
-  const supabase = await createClient();
+  const profile = (await fetchFromGo("/user/profile")) as UserType;
 
-  const [
-    profileResponse,
-    {
-      data: { user },
-    },
-  ] = await Promise.all([
-    fetchFromGo("/user/profile"),
-    supabase.auth.getUser(),
-  ]);
-
-  const profile = profileResponse as ProfileType;
-  const email = user?.email || "";
-  const avatar =
-    user?.user_metadata?.avatar_url ?? user?.user_metadata?.picture ?? null;
-
-  return (
-    <ProfileClient initialProfile={profile} email={email} avatar={avatar} />
-  );
+  return <ProfileClient initialProfile={profile} />;
 };
 
 export default ProfilePage;
