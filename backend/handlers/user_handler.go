6a16788da36331a1c73cb1a360be2a3ff1fd7c2f
Emmy <famuyiwaemmanuel565@gmail.com>
2026-09-11 22:27:30 +0100

    feat: add InviteAdminHandler and UpdateAdminProfile methods for admin management

--- backend/handlers/user_handler.go
+++ backend/handlers/user_handler.go
@@ -16,7 +16,9 @@ type UserHandler struct {
 }
 
 func NewUserHandler(db *pgxpool.Pool) *UserHandler {
-	return &UserHandler{DB: db}
+	return &UserHandler{
+		DB: db,
+	}
 }
 
 func (h *UserHandler) UpdateProfile(w http.ResponseWriter, r *http.Request) {
@@ -102,4 +104,75 @@ func (h *UserHandler) GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(users)
+}
+
+func (h *UserHandler) InviteAdminHandler(w http.ResponseWriter, r *http.Request) {
+	role, _ := r.Context().Value(middleware.UserRoleKey).(string)
+
+	userID, ok := r.Context().Value(middleware.UserIDKey).(string)
+	if !ok || userID == "" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	isAdmin := (role == "IT Admin")
+	if !isAdmin {
+		http.Error(w, "Forbidden: Only IT Admins can invite new admins", http.StatusForbidden)
+		return
+	}
+
+	type InviteRequest struct {
+		Email string `json:"email"`
+	}
+
+	var req InviteRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	err := repositories.InviteAdmin(r.Context(), h.DB, req.Email)
+	if err != nil {
+		log.Println("Admin invite error: %w\n", err)
+		http.Error(w, "Failed to process admin invitation", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Admin invited successfully"})
+}
+
+func (h *UserHandler) UpdateAdminProfile(w http.ResponseWriter, r *http.Request) {
+	role, _ := r.Context().Value(middleware.UserRoleKey).(string)
+
+	userID, ok := r.Context().Value(middleware.UserIDKey).(string)
+	if !ok || userID == "" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	isAdmin := (role == "IT Admin")
+	if !isAdmin {
+		http.Error(w, "Forbidden: Only IT Admins can update new details", http.StatusForbidden)
+		return
+	}
+
+	type UpdateProfileRequest struct {
+		Username string `json:"username"`
+	}
+	var req UpdateProfileRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	err := repositories.UpdateUserProfile(r.Context(), h.DB, userID, &req.Username, nil)
+	if err != nil {
+		log.Println("Admin invite error: %w\n", err)
+		http.Error(w, "Failed to process admin updating of profile", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Profile username updated"})
 }
